2117bdce0f2a7cc6fa291775a6973d2c8f84e0e3
Olga Demina <miserylab.r6s@gmail.com>
2022-07-27 11:57:46 +0300

    lesson 10: removed non-usable imports

--- demoqa_tests/app.py
+++ demoqa_tests/app.py
@@ -1,12 +1,8 @@
 __author__ = 'miserylab'
 
-import pytest
+
 from selene import have, command, browser
-import conftest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selene.support.shared import browser
-from selene import Browser
 from demoqa_tests.model.pages.modal_dialog import ModalDialog
 from demoqa_tests.model.pages.page import StudentRegistrationForm
 
@@ -16,20 +12,6 @@ class ApplicationManager:
         self.results = ModalDialog()
 
     def given_student_registration_form_opened(self, browser_):
-        # options = Options()
-        # selenoid_capabilities = {
-        #     "browserName": "chrome",
-        #     "browserVersion": "100.0",
-        #     "selenoid:options": {
-        #         "enableVNC": True,
-        #         "enableVideo": True
-        #     }
-        # }
-        # options.capabilities.update(selenoid_capabilities)
-        # driver = webdriver.Remote(
-        #     command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        #     options=options)
-        # browser.config.driver = driver
 
         browser.config.driver = browser_.driver
 

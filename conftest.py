6766a9ebb0aaca822f765811727dd7f71063b2e7
Olga Demina <miserylab.r6s@gmail.com>
2022-07-27 13:00:03 +0300

    lesson 10: change DEFAULT_BROWSER_VERSION

--- conftest.py
+++ conftest.py
@@ -19,13 +19,13 @@ def browser_management():
     browser.config.window_width = 1000
 
 
-DEFAULT_BROWSER_VERSION = "99"
+DEFAULT_BROWSER_VERSION = "100.0"
 
 
 def pytest_addoption(parser):
     parser.addoption(
         '--browser_version',
-        default='99'
+        default='100.0'
     )
 
 
